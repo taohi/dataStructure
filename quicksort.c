2b7c7b5b9a87bde4c11728fe2c172619f1a9c964
huang haitao <tao_08@qq.com>
2014-03-28 22:02:14 +0800

    最简单的快速排序算法

---
+++ quicksort.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void swap(int v[],int i,int j)
+{
+    int temp;
+    temp = v[i];
+    v[i]=v[j];
+    v[j]=temp;
+}
+
+void qsort(int v[],int left,int right)
+{
+    int i,last;
+    if(left >=right)
+        return;
+    swap(v,left,(left+right)/2);
+    last=left;
+    for(i = left+1;i <=right;i++)
+        if(v[i]<v[left])
+            swap(v,++last,i);
+    swap(v,left,last);
+    qsort(v,left,last-1);
+    qsort(v,last+1,right);
+}
+
+int main()
+{
+    int i=0;
+    int num[]={3,1,2,6,8,12,11,28,45,32,99};
+    qsort(num,0,10);
+    while(i<10)
+        printf("%d\t",num[i++]);
+    printf("\n");
+        
+}
