8629045a3b01c9eb3369ac3d046b83a756a34dab
huang haitao <tao_08@qq.com>
2014-04-07 19:15:35 +0800

    argv的第二版

--- argv.c
+++ argv.c
@@ -2,8 +2,10 @@
 main(int argc,char *argv[])
 {
     int i;
-    for(i=1;i<argc;i++)
-        printf("%s%s",argv[i],(i<argc-1)?" ":"");
+ //   for(i=1;i<argc;i++)
+//        printf("%s%s",argv[i],(i<argc-1)?" ":"");
+    while(--argc > 0)
+        printf("%s%s",*++argv,(argc > 1)?" ":"");
     printf("\n");
     return 0;
 }
