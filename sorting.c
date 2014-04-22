fc5dfae783fa90cdf961720b1b322ca54492ff09
huang haitao <tao_08@qq.com>
2014-04-22 12:56:42 +0800

    添加非递归的堆排序

--- sorting.c
+++ sorting.c
@@ -95,6 +95,33 @@ void shell_insert_sort(int *a,int n)
                 swap(a,j,j+gap);
 }
 
+//O(nlogn) Not stable. O(1) spare space.
+void heap_adjust(int *a,int i,int n)
+{
+    int child,temp;
+    for(temp=a[i];2*i+1 < n;i=child)
+    {
+        child = 2*i +1;
+        if(child!=n-1 && a[child+1]>a[child])
+            ++child;
+        if(temp >= a[child])
+            break;
+        a[i]=a[child];
+    }
+    a[i]=temp;
+}
+void heap_sort(int *a,int n)
+{
+    int i;
+    for(i=n/2 -1;i>=0;i--)
+        heap_adjust(a,i,n);
+    for(i=n-1;i>0;i--)
+    {
+        swap(a,0,i);
+        heap_adjust(a,0,i);
+    }
+}
+
 void print(int *a,int n)
 {
     int i;
@@ -111,10 +138,12 @@ void main()
     int a4[]={13,0,5,8,1,7,21,50,9,2};
     int a5[]={13,0,5,8,1,7,21,50,9,2};
     int a6[]={13,0,5,8,1,7,21,50,9,2};
+    int a7[]={13,0,5,8,1,7,21,50,9,2};
     bubble_sort(a1,10);     print(a1,10);
     select_sort(a2,10);     print(a2,10);
     quick_sort(a3,0,9);     print(a3,10);
     insertion_sort(a4,10);  print(a4,10);
     shell_insert_sort(a5,10);print(a5,10);
+    heap_sort(a6,10);       print(a6,10);
 
 }
