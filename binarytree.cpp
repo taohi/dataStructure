809bf205d0a3c6b33e891d7f0c785c9564988f83
taohi <taohi@taohi.net>
2014-08-30 15:30:56 +0800

    二叉树的各种操作

--- binarytree.cpp
+++ binarytree.cpp
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <queue>
+#include <stack>
+using namespace::std;
+
 //二叉树的结点结构体
 typedef struct tNode{
     int data;
@@ -7,76 +11,125 @@ typedef struct tNode{
     struct tNode *rchild;
 }treeNode;
 
-/*
-*以下函数中，涉及修改二叉树，用二级指针；
-*涉及统计、查找的，用一级指针就行。
-*这是函数传参特性,实参形参不同。
-*/
-
-//二叉树的先序遍历递归方法
-void PreOrderTraverse(treeNode *T)
+//二叉树的先序遍历,递归方法
+void preOrderTraverse(treeNode *T)
 {
     if(!T)
         return;
     printf("%d\t",T->data);
-    PreOrderTraverse(T->lchild);
-    PreOrderTraverse(T->rchild);
+    preOrderTraverse(T->lchild);
+    preOrderTraverse(T->rchild);
 }
 
-//二叉树的中序遍历递归方法
-void InOrderTraverse(treeNode * T)
+//二叉树的中序遍历,递归方法
+void inOrderTraverse(treeNode * T)
 {
     if(!T)
         return;
-    InOrderTraverse(T->lchild);
+    inOrderTraverse(T->lchild);
     printf("%d\t",T->data);
-    InOrderTraverse(T->rchild);
+    inOrderTraverse(T->rchild);
 }
 
-//二叉树的后序遍历递归方法
-void PostOrderTraverse(treeNode * T)
+//二叉树的后序遍历,递归方法
+void postOrderTraverse(treeNode * T)
 {
     if(!T)
         return;
-    PostOrderTraverse(T->lchild);
-    PostOrderTraverse(T->rchild);
+    postOrderTraverse(T->lchild);
+    postOrderTraverse(T->rchild);
     printf("%d\t",T->data);
 }
 
+//二叉树的先序遍历,栈方法
+void preOrderStack(treeNode *T)
+{
+    if(!T) 
+        return;
+    stack<treeNode *> s;
+    s.push(T);
+    while(!s.empty())
+    {
+        treeNode *p=s.top();
+        s.pop();
+        printf("%d\t",p->data);
+        if(p->rchild)
+            s.push(p->rchild);
+        if(p->lchild)
+            s.push(p->lchild);
+    }
+}
 
+//二叉树的中序遍历,栈方法
+void inOrderStack(treeNode *T)
+{
+    treeNode *p=T;
+    stack<treeNode *> s;
+    if(!p)
+        return;
+    while(!s.empty()||p!=NULL)
+    {
+        while(p!=NULL)
+        {
+            s.push(p);
+            p=p->lchild;
+        }
+        if(!s.empty())
+        {
+            p=s.top();
+            s.pop();
+            printf("%d\t",p->data);
+            p=p->rchild;
+        }
+    }
+}
 
-/*二叉树层序遍历（使用链式队列）
- *过程描述： 
- * 1.初始化一个队列queue;
- * 2.根结点T入队列;
- * 3.while(队列不为空):
- *  3.1:queue的队头q出队列;
- *  3.2:访问q的data域;
- *  3.3:如果q有左孩子，左孩子入队列;
- *  3.4:如果q有右孩子，右孩子入队列;
- *  使用C++的deque容器实现如下：
- * void levelTraverse(treeNode *T) 
- * {
- *      deque<treeNode *>nodeQueue;
- *      if(!T)
- *      {
- *          nodeQueue.push_back(T);
- *          while(!nodeQueue.empty())
- *          {
- *              treeNode *pHead=nodeQueue.at(0);
- *              printf("%d\t",pHead->data);
- *              nodeQueue.pop_front();
- *              if(pHead->lchild)
- *                  nodeQueue.push_back(pHead->lchild);
- *              if(pHead->rchild)
- *                  nodeQueue.push_back(pHead->rchild);
- *          }
- *      }
- * }
- */
+//二叉树后序遍历,栈方法
+void postOrderStack(treeNode *T)
+{
+    treeNode *cur=NULL;
+    treeNode *pre=NULL;
+    stack<treeNode *> s;
+    if(!T)
+        return;
+    s.push(T);
+    while(!s.empty())
+    {
+        cur=s.top();
+        if((cur->lchild==NULL && cur->rchild==NULL)||(pre!=NULL &&(pre==cur->lchild||pre==cur->rchild)))
+        {
+            printf("%d\t",cur->data);
+            s.pop();
+            pre=cur;
+        }
+        else
+        {
+            if(cur->rchild!=NULL)
+                s.push(cur->rchild);
+            if(cur->lchild!=NULL)
+                s.push(cur->lchild);
+        }
+    }
+}
 
+//二叉树层序遍历,队列方法
+void levelOrderTraverse(treeNode *T) 
+{
+     queue<treeNode *>nodeQueue;
+     if(!T) return;
+     nodeQueue.push(T);
+     while(!nodeQueue.empty())
+     {
+         treeNode *pHead=nodeQueue.front();
+         printf("%d\t",pHead->data);
+         nodeQueue.pop();
+         if(pHead->lchild)
+             nodeQueue.push(pHead->lchild);
+         if(pHead->rchild)
+             nodeQueue.push(pHead->rchild);
+     }
+}
 
-    
 //按值x大小插入，构建二叉排序树BST
 void insertBST(treeNode ** pT,int x)
 {
@@ -104,14 +157,14 @@ void insertBST(treeNode ** pT,int x)
 //判断值x是否在给定的二叉排序树中。
 int searchBST(treeNode *T,int x)
 {
-   if(T==NULL) 
-       return 0;
-   else if(T->data == x)
-       return 1;
-   else if(x < T->data)
-       return searchBST(T->lchild,x);
-   else
-       return searchBST(T->rchild,x);
+    if(T==NULL) 
+        return 0;
+    else if(T->data == x)
+        return 1;
+    else if(x < T->data)
+        return searchBST(T->lchild,x);
+    else
+        return searchBST(T->rchild,x);
 }
 
 //释放掉整个二叉树
@@ -127,54 +180,55 @@ void freeTree(treeNode **pT)
     return;
 }
 
-//统计二叉树的叶结点递归方法
+//统计二叉树的叶结点,递归方法
 int countLeaf(treeNode *T)
 {
     int counter=0;
-    if(T!=NULL)
-    {
-        counter+= countLeaf(T->lchild);
-        counter+= countLeaf(T->rchild);
-        if((T->lchild==NULL) && (T->rchild==NULL))
-            counter++;
-    }
-        return counter;
+    if(!T)
+        return 0;
+    if((T->lchild==NULL) && (T->rchild==NULL))
+        return 1;
+    counter+= countLeaf(T->lchild);
+    counter+= countLeaf(T->rchild);
+    return counter;
 }
 
-//计算二叉树的深度递归方法
+//计算二叉树的深度,递归方法
 int treeDepth(treeNode *T)
 {
     int ldepth=0;
     int rdepth=0;
     if(!T)
         return 0;
-    else
-    {
-        ldepth=treeDepth(T->lchild);
-        rdepth=treeDepth(T->rchild);
-    }
+    ldepth=treeDepth(T->lchild);
+    rdepth=treeDepth(T->rchild);
     if(ldepth>rdepth)
         return ldepth+1;
     else 
         return rdepth+1;
 }
 
-void main()
+int main()
 {
     int n[10] = {3,2,4,1,7,6,5,8,10,9};
     int i=0,find_key=8;
     treeNode *T=NULL;
-    //构建二叉排序树，依次插入数组元素
+    //构建平衡二叉排序树，依次插入数组元素
     while(i<10)
     {
         insertBST(&T,n[i]);
         i++;
     }
-    printf("PreOrder:\t"); PreOrderTraverse(T); printf("\n");
-    printf("InOrder:\t"); InOrderTraverse(T); printf("\n");
-    printf("PostOrder:\t"); PostOrderTraverse(T); printf("\n");
-    printf("tree depth:%d\n",treeDepth(T));
-    printf("tree leaves:%d\n",countLeaf(T));
+    printf("PreOrder(Recursive):\t"); preOrderTraverse(T); printf("\n");
+    printf("PreOrder(Stack):\t"); preOrderStack(T); printf("\n");
+    printf("InOrder(Recursive):\t"); inOrderTraverse(T); printf("\n");
+    printf("InOrder(Stack):\t\t"); inOrderStack(T); printf("\n");
+    printf("PostOrder(Recursive):\t"); postOrderTraverse(T); printf("\n");
+    printf("PostOrder(Stack):\t"); postOrderStack(T); printf("\n");
+    printf("LevelOrder(Queue):\t"); levelOrderTraverse(T); printf("\n");
+    printf("tree depth(Recursive):\t%d\n",treeDepth(T));
+    printf("tree leaves(Recursive):\t%d\n",countLeaf(T));
     printf("%d is %s in BST tree.\n",find_key,searchBST(T,find_key)?"":"not");
     freeTree(&T);
+    return 0;
 }
